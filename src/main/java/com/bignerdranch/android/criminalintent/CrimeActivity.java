8e22e46b89c87dc7a9e52104c14a4cb2a8fbca5f
alex <alexanderpushkar86@gmail.com>
2017-10-01 01:14:35 +0900

    Make CrimeFragment independent of CrimeActivity.

--- src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -5,10 +5,15 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
